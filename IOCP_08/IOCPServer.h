bfd43524bccebd1011029aa568ade6500ac85e66
YoonJuK <silverlnng0721@gmail.com>
2025-12-24 18:41:09 +0900

    ipch 파일 gitignore처리

--- IOCP_08/IOCPServer.h
+++ IOCP_08/IOCPServer.h
@@ -11,9 +11,28 @@ class IOCPServer
 public:
 
 	//  소켓 초기화
-	bool Init()
+	bool Init(const UINT32 maxIOWorkerThreadCount_)
 	{
-		WSASocket
+		WSADATA wsaData;
+
+		// 
+		int nResult = WSAStartup();
+
+		// 연결지향형 TCP ,Overlapped IO 소켓을생성
+		mListenSocket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, NULL, WSA_FLAG_OVERLAPPED);
+
+		if (mListenSocket == INVALID_SOCKET)
+		{
+			printf("[에러] WSASocket () 함수 실패 : %d\n", WSAGetLastError());
+			return false;
+		}
+
+		MaxIOWorkerThreadCount = maxIOWorkerThreadCount_;
+
+		printf("소켓 초기화 성공\n");
+
+		return true;
+
 	}
 
 	// 서버의 주소정버와 소켓과 연결시키고 접속 요청을 받기위해 소켓을 등록 
@@ -40,8 +59,78 @@ public:
 
 private:
 
+	void CreateClient()
+	{
+
+	}
+
+	bool CreateWorkerThread()
+	{
+
+	}
+
+	bool CreateAccepterThread()
+	{
+
+	}
+
+	void WorkerThread()
+	{
+
+	}
+
+	void AccepterThread()
+	{
+		while (mIsAccepterRun)
+		{
+			// 현재시간을 초(second) 단위의 정수로 변환 
+			auto curTimeSec = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
+
+			for (auto client : mClientInfos)
+			{
+				// 클라이언트가 이미 연결되어있으면 바로 다음 반복문으로 
+				if (client->IsConnected())
+				{
+					continue;
+				}
+
+				
+				if ((UINT64)curTimeSec < client->GetLatestClosedTimeSec())
+				{
+					continue;
+				}
+
+				// 재사용 대기시간 체크 ( 중요 !! )
+					// 어떤 클라이언트가 접속을 끊자마자 해당 메모리 공간(객체)을 즉시 다른 사람에게 할당하면, 네트워크 상에서 지연 도착한 이전 사람의 패킷이 새 사람의 패킷으로 오인될 수 있는 위험이 있습니다 (TCP의 Time-Wait 문제와 유사).
+
+				auto diff = curTimeSec - client->GetLatestClosedTimeSec();
+
+				if (diff <= RE_USE_SESSION_WAIT_TIMESEC)
+				{
+					continue;
+				}
+
+				// 리슨 소켓과 현재 시간을 전달
+				client->PostAccept(mListenSocket, curTimeSec);
+			}
+
+			std::this_thread::sleep_for(std::chrono::milliseconds(32));
+
+			// 1초/32 만큼 쉬다가 다시 확인
+			// => 1초에 32 번 확인하게 됨
+		}
+	}
+
+	void CloseSocket()
+	{
+
+	}
+
+
+	UINT32 MaxIOWorkerThreadCount = 0;
+
 	// 클라이언트 정보 저장 구조체
-	//std::vector<stClientIn>
+	std::vector<stClientInfo*> mClientInfos;
 
 	// 클라이언트 접속을 받기위한 리슨 소켓
 	SOCKET mListenSocket = INVALID_SOCKET;
@@ -62,6 +151,6 @@ private:
 	bool	mIsWorkerRun = true;
 
 	// 접속 쓰레드 동작 플래드
-	bool	mIsAcceptRun = true;
+	bool	mIsAccepterRun = true;
 };
 
--- IOCP_08/Packet.h
+++ IOCP_08/Packet.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#define WIN32_LEAN_AND_MEAN
+// Windows API 헤더 파일을 포함하기 전에 선언하여 암호화, DDE, RPC, 셸 및 Windows 소켓과 같이 덜 자주 사용되는 API를 제외시켜 헤더 파일의 크기를 줄이고 컴파일 속도를 향상시키는 전처리기 지시문
+
 #include <windows.h>
+
 
 struct PacketInfo
 {
