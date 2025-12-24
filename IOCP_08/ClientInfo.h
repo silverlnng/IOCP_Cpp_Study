6a10f32e20d7c8e201b35feff425a41e21ee4c0f
YoonJuK <silverlnng0721@gmail.com>
2025-12-24 18:33:37 +0900

    Revert "클라이언트 AcceptEX 으로 비동기로 accept 예약걸기"
    This reverts commit d4833dacbde6d0bc7332427a360cc0e2ac239f90.

--- IOCP_08/ClientInfo.h
+++ IOCP_08/ClientInfo.h
@@ -33,31 +33,8 @@ public:
 
 	bool OnConnect()
 	{
-	}
 
-	void Close()
-	{
-
-
-	}
-
-	bool PostAccept(SOCKET listenSock_,const UINT64 curTimeSec_)
-	{
-		// 미리 접속할 빈소켓을 만들고 운영체제에 예약해두는 역할
-
-		//  닫힌 시간을 최대값으로 초기화
-		mLatestClosedTimeSec = UINT32_MAX;
-
-		// mSocket 을 소켓을 미리 만들기 
-		mSocket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_IP, NULL, 0, WSA_FLAG_OVERLAPPED);
-
-		// 비동기 접속 예약 AcceptEx : 이 함수는 이 함수는 운영체제에게 **"이 listenSock_으로 누가 접속하면, 내가 미리 만든 mSocket하고 연결해 주세요."**라고 부탁하는 함수입니다.
-
-		// AcceptEx() : 4번째 인자가 0인 것은 **"접속만 시키고, 첫 데이터는 기다리지 말라"**는 뜻입니다.
-	}
-
-	bool AcceptCompletion()
-	{
+		
 
 	}
 
