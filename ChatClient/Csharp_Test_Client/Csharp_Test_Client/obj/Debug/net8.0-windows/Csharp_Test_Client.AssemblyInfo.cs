ddde3db814f63eacb12980b2b25aa7c87365bb0f
YoonJuK <silverlnng0721@gmail.com>
2026-02-03 12:53:22 +0900

    Release 모드도 빌드되도록 세팅(redis,mysql)

--- ChatClient/Csharp_Test_Client/Csharp_Test_Client/obj/Debug/net8.0-windows/Csharp_Test_Client.AssemblyInfo.cs
+++ ChatClient/Csharp_Test_Client/Csharp_Test_Client/obj/Debug/net8.0-windows/Csharp_Test_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp_Test_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2c646b7cabb0d243b1c899b983148d41e1bbc26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2d71a75f928fbc9efd185363cfc1f77fbdc5f98")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp_Test_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp_Test_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
