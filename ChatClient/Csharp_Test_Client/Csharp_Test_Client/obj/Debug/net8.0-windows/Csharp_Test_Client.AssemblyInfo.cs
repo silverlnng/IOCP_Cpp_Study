8c4f8b0043b7e8b34061e7cb5ad6850d6b5361fc
YoonJuK <silverlnng0721@gmail.com>
2026-02-03 11:45:35 +0900

    max client 15000 으로 수정

--- ChatClient/Csharp_Test_Client/Csharp_Test_Client/obj/Debug/net8.0-windows/Csharp_Test_Client.AssemblyInfo.cs
+++ ChatClient/Csharp_Test_Client/Csharp_Test_Client/obj/Debug/net8.0-windows/Csharp_Test_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp_Test_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2c646b7cabb0d243b1c899b983148d41e1bbc26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a173c8f7a987e51ae32770daf4a5ccf933f2eda0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp_Test_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp_Test_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
